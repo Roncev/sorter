8703c2e3da3fb3bf5f110a74fb814ffe9cbca45c
moteo <moteoo@gmail.com>
2018-02-20 01:49:08 +0300

    7 passing

--- src/index.js
+++ src/index.js
@@ -1,30 +1,40 @@
 class Sorter {
   constructor() {
+    this.arr = [];
+    console.log(indices)
+
     // your implementation
   }
 
   add(element) {
+    this.arr.push(element);
     // your implementation
   }
 
   at(index) {
+    return this.arr[index];
     // your implementation
   }
 
   get length() {
+    return this.arr.length;
     // your implementation
   }
 
   toArray() {
+    return this.arr
     // your implementation
   }
 
   sort(indices) {
+    return this.arr.sort(function(x,y) {return x-y})
+    console.log(indices)
     // your implementation
   }
 
   setComparator(compareFunction) {
-    // your implementation
+    console.log(indices)
+
   }
 }
 
